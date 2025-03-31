958901c72043c7f2a80f7c13afa8eb8b2aa40fcc
Марина Бельмесова <bem@jcat.ru>
2025-03-31 14:21:52 +0300

    Команда поставлена в очередь, но для демонстрации будем запускать команду перед выводом новостей

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 use App\Http\Filters\FeedFilter;
 use App\Http\Requests\NewsRequest;
 use App\Models\Feed;
+use Illuminate\Support\Facades\Artisan;
 
 class NewsController extends Controller
 {
     const MAX_ITEMS = 10;
 
     public function index(NewsRequest $request) {
+        // Запускаем команду app:import-feeds
+        Artisan::call('app:import-feeds');
+
         $filter = app()->make(FeedFilter::class, ['queryParams' => $request->validated()]);
         $page = $request->query('page', 1);
 
